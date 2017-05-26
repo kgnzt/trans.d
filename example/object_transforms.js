1fca01961f5f421a1103397a13723040d2a1b3b1
Nicolas Feigenbaum <feigenbaum.nicolas@gmail.com>
2017-05-26 15:10:35 -0700

    update object transform example

--- example/object_transforms.js
+++ example/object_transforms.js
@@ -14,10 +14,10 @@ const transform = transd.compose(
         delta: 5
       };
 
-const array = transd.into(transform, [], input),
+const array  = transd.into(transform, [], input),
       object = transd.into(transform, {}, input),
-      map = transd.into(transform, new Map(), input),
-      set = transd.into(transform, new Set(), input),
+      map    = transd.into(transform, new Map(), input),
+      set    = transd.into(transform, new Set(), input),
       string = transd.into(transform, '', input),
       number = transd.into(transform, 0, input);
 
@@ -25,5 +25,5 @@ console.log('Array:  ', array);  // [ 2, 6, 6 ]
 console.log('Object: ', object); // { alpha_update: 2, gamma_update: 6, delta_update: 6 }
 console.log('Map:    ', map);    // Map { 'alpha_update' => 2, 'gamma_update' => 6, 'delta_update' => 6 }
 console.log('Set:    ', set);    // Set { 2, 6 }
-console.log('String: ', string); // 266
+console.log('String: ', string); // '266'
 console.log('Number: ', number); // 14
